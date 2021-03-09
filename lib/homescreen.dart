62ee98abed19b8acfbb56315b3fb5d511300d41d
Philipp Hinzmann <philipp-hinzmann@web.de>
2021-03-09 17:21:31 +0100

    Erster Entwurf

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,8 +1,27 @@
+import 'styles.dart';
 import 'package:flutter/material.dart';
 
-class Homescreen extends StatelessWidget {
+class HomescreenList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(title: Text("Übersicht")),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _function("Kalender"),
+            _function("Tagebuch"),
+            _function("Physiotherapie")
+          ],
+        ));
+  }
+
+  Widget _function(String title) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(40.0, 40.0, 40.0, 40.0),
+      child:
+          Text(title, textAlign: TextAlign.center, style: Styles.textDefault),
+    );
   }
 }
