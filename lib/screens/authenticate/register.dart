230bbffef0c3f815067ff72894f1730b0eebd228
Philipp Hinzmann <philipp-hinzmann@web.de>
2021-04-17 15:01:00 +0200

    ListView Eingefügt

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -42,61 +42,68 @@ class _RegisterState extends State<Register> {
                 ),
               ],
             ),
-            body: Container(
-              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(height: 20.0),
-                    TextFormField(
-                      decoration: Styles.textInputDecoration
-                          .copyWith(hintText: 'E-Mail'),
-                      validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                      onChanged: (val) {
-                        setState(() => email = val);
-                      },
-                    ),
-                    SizedBox(height: 20.0),
-                    TextFormField(
-                      decoration: Styles.textInputDecoration
-                          .copyWith(hintText: 'Password'),
-                      obscureText: true,
-                      validator: (val) => val.length < 6
-                          ? 'Enter a password 6+ chars long'
-                          : null,
-                      onChanged: (val) {
-                        setState(() => password = val);
-                      },
-                    ),
-                    SizedBox(height: 20.0),
-                    ElevatedButton(
-                        //color: Colors.pink[400],
-                        child: Text(
-                          'Register',
-                          style: TextStyle(color: Colors.white),
+            body: ListView(
+              children: [
+                Container(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 20.0),
+                        TextFormField(
+                          decoration: Styles.textInputDecoration
+                              .copyWith(hintText: 'E-Mail'),
+                          validator: (val) =>
+                              val.isEmpty ? 'Enter an email' : null,
+                          onChanged: (val) {
+                            setState(() => email = val);
+                          },
                         ),
-                        onPressed: () async {
-                          if (_formKey.currentState.validate()) {
-                            setState(() => loading = true);
-                            dynamic result = await _auth
-                                .registerWithEmailAndPassword(email, password);
-                            if (result == null) {
-                              setState(() {
-                                error = 'Please supply a valid email';
-                                loading = false;
-                              });
-                            }
-                          }
-                        }),
-                    SizedBox(height: 12.0),
-                    Text(
-                      error,
-                      style: TextStyle(color: Colors.red, fontSize: 14.0),
-                    )
-                  ],
+                        SizedBox(height: 20.0),
+                        TextFormField(
+                          decoration: Styles.textInputDecoration
+                              .copyWith(hintText: 'Password'),
+                          obscureText: true,
+                          validator: (val) => val.length < 6
+                              ? 'Enter a password 6+ chars long'
+                              : null,
+                          onChanged: (val) {
+                            setState(() => password = val);
+                          },
+                        ),
+                        SizedBox(height: 20.0),
+                        ElevatedButton(
+                            //color: Colors.pink[400],
+                            child: Text(
+                              'Register',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            onPressed: () async {
+                              if (_formKey.currentState.validate()) {
+                                setState(() => loading = true);
+                                dynamic result =
+                                    await _auth.registerWithEmailAndPassword(
+                                        email, password);
+                                if (result == null) {
+                                  setState(() {
+                                    error = 'Please supply a valid email';
+                                    loading = false;
+                                  });
+                                }
+                              }
+                            }),
+                        SizedBox(height: 12.0),
+                        Text(
+                          error,
+                          style: TextStyle(color: Colors.red, fontSize: 14.0),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
           );
   }
