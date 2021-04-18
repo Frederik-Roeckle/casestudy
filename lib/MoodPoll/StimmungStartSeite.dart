15539b90a432756032e4542867be551612c73c29
Frederik-Roeckle <78876142+Frederik-Roeckle@users.noreply.github.com>
2021-04-18 17:03:52 +0200

    Screen/statistic mood (#28)
    * changed Mood Structure, first statistic screen
    
    * first functional prototype of mood statistic
    
    * edit size of smiley in chart legend

---
+++ lib/MoodPoll/StimmungStartSeite.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_casestudy/styles.dart';
+
+class StimmungStartSeite extends StatefulWidget {
+  @override
+  _StimmungStartSeiteState createState() => _StimmungStartSeiteState();
+}
+
+class _StimmungStartSeiteState extends State<StimmungStartSeite> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("Stimmung", style: Styles.headerLarge),
+            backgroundColor: Styles.appBarColor),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            _routingTile(context, 'Stimmungsabfrage', '/MoodPoll'),
+            _routingTile(context, 'Statistik', '/Stimmung-Statistik'),
+          ],
+        ));
+  }
+
+  Widget _routingTile(context, String funktion, String route) {
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        child: Container(
+          padding: EdgeInsets.all(30.0),
+          decoration: BoxDecoration(
+              color: Styles.STRONG_GREEN,
+              borderRadius: BorderRadius.all(Radius.circular(20))),
+          child: new TextButton(
+              onPressed: () => Navigator.pushNamed(context, route),
+              child: new Text(funktion, style: Styles.textDefault)),
+        )
+    );
+  }
+}
